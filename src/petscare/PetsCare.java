58a336f906d006755e8401ef1e75092d6170621a
Maryam AlBugaey <126514202+mtalbugaey@users.noreply.github.com>
2023-03-04 12:04:48 +0300

    Add files via upload

---
+++ src/petscare/PetsCare.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package petscare;
+
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import petscare.Classes.Product;
+
+/**
+ *
+ * @author Maryam Tariq
+ */
+public class PetsCare {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Login login = new Login();
+        login.setVisible(true); 
+    }
+    
+}
